4d8aa1323bc0de3e4dc0e35de2669163702ecc5d
Kevin Kang <veggente@gmai.com>
2015-07-31 16:51:00 -0700

    Clean up

--- groupem/templatetags/show_users.py
+++ groupem/templatetags/show_users.py
@@ -1,37 +1,3 @@
-"""from django.contrib.auth.models import User
-from django.contrib.sessions.models import Session
-from django.utils import timezone
-
-
-from bugtracker.models import Counter
-
-
-
-def get_all_logged_in_users():
-    This function gets the number of logged-in users
-
-    This is given as an answer by T. Stone at
-    http://stackoverflow.com/questions/2723052/how-to-get-the-list-of-the-authenticated-users
-
-    # Query all non-expired sessions.
-    # Use timezone.now() instead of datatime.now() in
-    # latest versions of Django.
-    sessions = Session.objects.filter(expire_date__gte=timezone.now())
-    uid_list = []
-    # Build a list of user ids from that query.
-    for session in sessions:
-        data = session.get_decoded()
-        uid_list.append(data.get('_auth_user_id', None))
-    # Query all logged-in users based on id list.
-    return User.objects.filter(id__in=uid_list)
-
-@register.inclusion_tag('bugtracker/'
-                        'logged_in_user_list.html')
-def render_logged_in_user_list():
-    hit_counter, created = Counter.objects.get_or_create(
-        name='hitcount')
-    return {'users': get_all_logged_in_users(), 'hitcount': hit_counter.count}
-"""
 from django import template
 register = template.Library()
 
