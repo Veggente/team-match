01a6aa506c90f4484b72462d0fb7650b8a214c16
Kevin Kang <veggente@gmai.com>
2015-07-31 14:07:16 -0700

    Link signup page but hyperref does not work yet

--- groupem/views.py
+++ groupem/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'groupem/index.html')
+
+def signup(request):
+    return render(request, 'groupem/signup.html')
